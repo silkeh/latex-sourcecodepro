be77e000a844cbaeffea07b13ad2483ff09aa2d0
Silke <silke@slxh.eu>
2015-05-13 16:47:24 +0200

    Tabs to spaces. Font features list removed

--- rebuild.sh
+++ rebuild.sh
@@ -12,21 +12,21 @@ font=${fontname,,}
 mv tex/latex/$font/$font.sty tex/latex/$font/$font.sty.tmp
 
 # Convert the OpenType fonts
-autoinst fonts/opentype/$vend/$font/*	\
-	-typewriter							\
-	-target=.							\
-	-vendor="$vend"						\
-	-typeface="$font"					\
-	-encoding=OT1,T1,LY1,TS1			\
-	-ts1								\
-	-nosmallcaps						\
-	-superiors							\
-	-inferiors							\
-	-fractions							\
-	-noswash							\
-	-notitling							\
-	-noornaments						\
-	-noupdmap
+autoinst fonts/opentype/$vend/$font/*   \
+    -typewriter                         \
+    -target=.                           \
+    -vendor="$vend"                     \
+    -typeface="$font"                   \
+    -encoding=OT1,T1,LY1,TS1            \
+    -ts1                                \
+    -nosmallcaps                        \
+    -superiors                          \
+    -inferiors                          \
+    -fractions                          \
+    -noswash                            \
+    -notitling                          \
+    -noornaments                        \
+    -noupdmap
 
 # Move the generated file and the hand-written one back
 mv tex/latex/$font/$fontname.sty tex/latex/$font/$font-type1-autoinst.sty
